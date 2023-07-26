8a5e0c60d0c7b3c45c79232c792b7beea4ea22fe
Robert Musili <robersi366@gmail.com>
2023-07-26 23:18:42 +0300

    Done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const taskForm = document.getElementById("create-task-form");
+  const taskInput = document.getElementById("new-task-description");
+  const taskList = document.getElementById("tasks");
+
+  taskForm.addEventListener("submit", (e) => {
+    e.preventDefault(); 
+    const taskText = taskInput.value.trim();
+
+    if (taskText !== "") {
+      const taskItem = document.createElement("li");
+      taskItem.innerText = taskText;
+      taskList.appendChild(taskItem);
+      taskInput.value = "";
+    }
+  });
 });
